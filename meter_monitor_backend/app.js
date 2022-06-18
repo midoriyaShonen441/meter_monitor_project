9f5460ffcff11911cc3e3f20de18e24ec8281b38
“midoriyaShonen441” <khunnatta@gmail.com>
2022-06-18 15:57:03 +0700

    add upload middleware

--- meter_monitor_backend/app.js
+++ meter_monitor_backend/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const imageToBase64 = require("image-to-base64");
+const upload = require("./modules/uploadMiddleware")
 require("dotenv").config();
 
 const app = express()
@@ -8,8 +9,11 @@ app.use(express.json());
 app.use(cors());
 
 // writing image to db
-app.post("/image/upload", async(req, res) => {
-    const image = req.body;
+app.get("/", async(req, res) => {
+    res.send("OK")
+})
+app.post("/image/upload", upload.single("file"), async(req, res) => {
+    res.send(req.file)
 })
 
 module.exports = app
