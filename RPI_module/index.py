b6f8f3b58c622ee6677634ef9cfb6d9298c8559f
“midoriyaShonen441” <khunnatta@gmail.com>
2022-10-20 02:33:19 +0700

    feat: add raspberry pi image upload module

--- RPI_module/index.py
+++ RPI_module/index.py
@@ -9,21 +9,44 @@ class connection:
         self.url = url
 
     async def upload(self, file):
+        url = self.url + "/image/upload"
         async with aiohttp.ClientSession() as session:
-            async with session.post(self.url) as res:
-                result = await res.json()
+            async with session.post(url, data=file) as res:
+                result = await res.text()
+                print(result)
+
+    # async def upload(self, file):
+    #     url = self.url + "/image/upload"
+    #     with open(file, "rb") as f:
+    #         print(f)
+            # async with aiohttp.ClientSession() as session:
+            #     async with session.post(url, data=f) as res:
+            #         print(await res.text())
+
+    async def test_debug(self):
+        url = self.url + "/"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(self.url) as res:
+                result = await res.text()
                 print(result)
 
 
-server = connection("http://localhost:3000/image/upload")
-filepath = r"./img"
-filenames = os.listdir(filepath)
+server = connection("http://localhost:3000")
+file_path = r"./img/"
+file_names = os.listdir(file_path)
 
-for file in filenames:
-    print(file)
-    asyncio.run(server.upload(file))
-    # file = {
-    #     'file': (file, open(filepath, 'rb')),
-    #     'Content-Type': 'image/png',
-    #     'Content-Length': l
-    # }
+for file_name in file_names:
+    file = file_path + file_name
+    data = aiohttp.FormData()
+    data.add_field('file',
+                   open(file, 'rb'),
+                   filename=file_name,
+                   content_type='image/png')
+    # server.upload(data)
+#     files = {
+#         'file': open(file, 'rb'),
+#         'filename': file_name,
+#         'path': file
+#     }
+# print(files)
+    asyncio.run(server.upload(data))
